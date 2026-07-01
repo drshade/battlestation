edfd0a4bfa943b4bcbb4af0cdc9abe9f896b919c
tom <tom@synthesis.co.za>
2026-07-01 22:50:45 +0200

    bunch of small changes

--- stow/home/fish/.config/fish/config.fish
+++ stow/home/fish/.config/fish/config.fish
@@ -6,3 +6,5 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 export PATH="$HOME/.local/bin:$PATH"
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/tom/.ghcup/bin # ghcup-env
