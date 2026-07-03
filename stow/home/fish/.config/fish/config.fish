9e8dcee9e97f9fcf16aa9a51b0d28ed9e1d130cf
tom <tom@synthesis.co.za>
2026-07-03 11:47:46 +0200

    Add battlestation fastfetch logo

--- stow/home/fish/.config/fish/config.fish
+++ stow/home/fish/.config/fish/config.fish
@@ -1,10 +1,14 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+    fastfetch \
+        --pipe false \
+        --kitty "$HOME/.config/fish/battlestation-logo.png" \
+        --logo-preserve-aspect-ratio \
+        --logo-width 40 \
+        --logo-padding 2 \
+        --logo-padding-top 4
+end
 export PATH="$HOME/.local/bin:$PATH"
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/tom/.ghcup/bin # ghcup-env
